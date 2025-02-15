62ec55d485d893253b2cbcc8bee123f655baa234
Lilit Mkrtchyan <67322020+mkrtchyanlilit@users.noreply.github.com>
2025-02-15 14:44:23 +0400

    Add files via upload
    Changed functions logic

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 
+//GET TODOS
 async function fetchTodos() {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos?userId=1"
+    );
     const todos = await response.json();
-    let first20 = todos.slice(0, 20);
-    first20.forEach((todo) => {
+    taskList.innerHTML = "";
+    todos.forEach((todo) => {
       const li = createTaskElement(todo.title, todo.completed);
       taskList.appendChild(li);
     });
@@ -16,11 +19,14 @@ async function fetchTodos() {
   }
 }
 
-function createTaskElement(taskText, isCompleted) {
+function createTaskElement(taskText, isCompleted, id) {
   const li = document.createElement("li");
+  li.setAttribute("data-id", id);
+
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.checked = isCompleted;
+
   const p = document.createElement("p");
   p.textContent = taskText;
   if (isCompleted) {
@@ -30,20 +36,35 @@ function createTaskElement(taskText, isCompleted) {
   checkbox.addEventListener("change", () => {
     p.classList.toggle("completed", checkbox.checked);
   });
+
   li.appendChild(checkbox);
   li.appendChild(p);
 
   const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "x";
+  deleteBtn.textContent = "Delete";
+  deleteBtn.style.color = "red";
+
+  const editBtn = document.createElement("button");
+  editBtn.textContent = "Edit";
+  editBtn.style.color = "green";
+
   deleteBtn.addEventListener("click", () => {
     taskList.removeChild(li);
   });
 
+  editBtn.addEventListener("click", () => {
+    // const taskId = li.getAttribute("data-id");
+    // updateTask(taskId, li);
+    console.log("updating");
+  });
+
+  li.appendChild(editBtn);
   li.appendChild(deleteBtn);
   return li;
 }
 
-function addTask() {
+//ADD TODOS
+async function addTask() {
   const taskText = taskInput.value.trim();
 
   if (taskText === "") {
@@ -51,9 +72,23 @@ function addTask() {
     return;
   }
 
-  const li = createTaskElement(taskText, false);
-  taskList.appendChild(li);
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos?userId=1",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        title: taskText,
+        completed: false,
+      }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    }
+  );
 
+  const task = await response.json();
+  const li = createTaskElement(task.title, task.completed);
+  taskList.appendChild(li);
   taskInput.value = "";
 }
 
@@ -64,5 +99,37 @@ taskInput.addEventListener("keypress", (e) => {
     addTask();
   }
 });
+
+
+//UPDATE TODOS
+// async function updateTask(id, li) {
+//   const taskText = taskInput.value.trim();
+
+//   const checkbox = li.querySelector("input[type='checkbox']");
+//   const updatedTask = {
+//     userId: 1,
+//     id: id,
+//     title: taskText,
+//     completed: checkbox.checked,
+//   };
+
+//   const response = await fetch(
+//     `https://jsonplaceholder.typicode.com/todos/${id}`,
+//     {
+//       method: "PUT",
+//       body: JSON.stringify(updatedTask),
+//       headers: {
+//         "Content-type": "application/json",
+//       },
+//     }
+//   );
+//   const task = await response.json();
+
+//   const p = li.querySelector("p");
+//   p.textContent = task.title;
+//   checkbox.checked = task.completed;
+//   p.classList.toggle("completed", task.completed);
+//   taskInput.value = "";
+// }
 
 fetchTodos();
